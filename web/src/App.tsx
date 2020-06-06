852d853095e8201cde794382476dbf756aaaec54
Maicon Costa <maiconcostawd@hotmail.com>
2020-06-06 10:44:51 -0300

    Finished create points

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.scss';
 
+import Routes from './routes';
+
 function App() {
   return (
     <div className="App">
-        <h1>Hello World</h1>
+        <Routes />
     </div>
   );
 }
